607974077840f05a77e247320efed68af89e1d94
calebjo <caleb.ri.jones@gmail.com>
2022-05-07 15:38:58 -0400

    Refactor game logic to include main menu screen, score count

--- html/src/com/badlogic/drop/client/HtmlLauncher.java
+++ html/src/com/badlogic/drop/client/HtmlLauncher.java
@@ -1,9 +1,9 @@
 package com.badlogic.drop.client;
 
+import com.badlogic.drop.Drop;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
-import com.badlogic.drop.Drop;
 
 public class HtmlLauncher extends GwtApplication {
 
